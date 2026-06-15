12a818cd4926a76988e5f8652fd257541269f118
hyein_tj <xzxz1006@gmail.com>
2026-06-15 16:57:27 +0900

    LemonSqueezyRequestDto 주석 수정

--- src/main/java/com/clearticket/clearticket/model/dto/LemonSqueezyRequestDto.java
+++ src/main/java/com/clearticket/clearticket/model/dto/LemonSqueezyRequestDto.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LemonSqueezyRequestDto {
-    private Long reservationId;     // 연관된 예약 고유 ID
-    private Long amount;            // 결제할 총 금액
+    private Long reservationId; // 연관된 예약 고유 ID
+    private Long amount; // 결제할 총 금액
     private String performanceTitle; // 가상 결제창에 띄워줄 공연 제목
 }
