985a46a2682dfcbeeaf631426fa3df37647fffce
hyein_tj <xzxz1006@gmail.com>
2026-06-16 17:57:50 +0900

    ReservationRepository - 메서드 명 변경

--- src/main/java/com/clearticket/clearticket/repository/ReservationRepository.java
+++ src/main/java/com/clearticket/clearticket/repository/ReservationRepository.java
@@ -14,12 +14,12 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
      * @param status 찾고자 하는 예매 상태
      * @return 상태 필터링이 완료된 예매 내역 리스트
      */
-    List<Reservation> findByUserIdAndStatus(Long userId, ReservationStatus status);
+    List<Reservation> findByUserUserIdAndStatus(Long userId, ReservationStatus status);
 
     /**
      * 특정 회원이 구매한 모든 예매 내역 전체 조회
      * @param userId 조회할 회원 ID
      * @return 해당 회원이 가진 전체 예매 내역 리스트
      */
-    List<Reservation> findByUserId(Long userId);
+    List<Reservation> findByUserUserId(Long userId);
 }
