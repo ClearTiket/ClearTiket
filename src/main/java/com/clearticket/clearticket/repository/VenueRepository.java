10d369bae414063242c896cd806dec1ae0b4fe08
dnsrlrla <dnsrlrla7@gmail.com>
2026-06-23 16:33:57 +0900

    find by id, find by region

--- src/main/java/com/clearticket/clearticket/repository/VenueRepository.java
+++ src/main/java/com/clearticket/clearticket/repository/VenueRepository.java
@@ -1,7 +1,12 @@
 package com.clearticket.clearticket.repository;
 
 import com.clearticket.clearticket.model.entity.Venue;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VenueRepository extends JpaRepository<Venue,Long> {
+import java.util.List;
+
+public interface VenueRepository extends JpaRepository<Venue, Long> {
+    Venue findByVenueId(long id);
+    List<Venue> findAllByRegion(String region, Pageable pageable);
 }
