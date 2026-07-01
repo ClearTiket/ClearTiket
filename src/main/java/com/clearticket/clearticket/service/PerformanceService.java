9c15eed0b06b43abe6def42f328bbe847ff3d02f
dnsrlrla <dnsrlrla7@gmail.com>
2026-07-01 12:34:36 +0900

    미사용 메서드 제거

--- src/main/java/com/clearticket/clearticket/service/PerformanceService.java
+++ src/main/java/com/clearticket/clearticket/service/PerformanceService.java
@@ -85,11 +85,6 @@ public class PerformanceService {
 
         List<String> regions = new ArrayList<>();
         switch (region) {
-            // TODO: 지역 텍스트 형식이 Venue 테이블과 Performances가 서로 다름 -> 이거 해결해야됨
-            //case "충청" -> regions.addAll(Arrays.asList("충북", "충남", "대전", "세종"));
-            //case "전라" -> regions.addAll(Arrays.asList("전북", "전남", "광주"));
-            //case "경상" -> regions.addAll(Arrays.asList("경북", "경남", "부산", "대구", "울산"));
-            case "서울" -> regions.add("서울특별시");
             case "충청" -> regions.addAll(Arrays.asList("충청북도", "충청남도", "대전광역시", "세종특별자치시"));
             case "전라" -> regions.addAll(Arrays.asList("전북특별자치도", "전라남도", "광주광역시"));
             case "경상" -> regions.addAll(Arrays.asList("경상북도", "경상남도", "부산광역시", "대구광역시", "울산광역시"));
@@ -97,10 +92,5 @@ public class PerformanceService {
         }
 
         return performanceRepository.findAllByRegionIn(regions, validPageable(page));
-    }
-
-    public Page<Performance> findRankingAllByGenre(String genre, Integer page) {
-        // TODO: 현재는 단순 장르 필터링만 -> 예매율 계산 + 정렬 로직 작성 필요
-        return performanceRepository.findAllByGenre(genre, validPageable(page, 10));
     }
 }
