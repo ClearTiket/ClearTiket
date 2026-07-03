b94599d48649617fda24a9ad43fe5eb354ef7689
dnsrlrla <dnsrlrla7@gmail.com>
2026-07-03 17:40:30 +0900

    메인 페이지에서 로그인 중인 경우 추천 공연 목록을 검색하여 표시하도록 코드 작성

--- src/main/java/com/clearticket/clearticket/repository/UserTagRepository.java
+++ src/main/java/com/clearticket/clearticket/repository/UserTagRepository.java
@@ -3,5 +3,8 @@ package com.clearticket.clearticket.repository;
 import com.clearticket.clearticket.model.entity.UserTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserTagRepository extends JpaRepository<UserTag, Long> {
+    List<UserTag> findAllByUserUserId(Long userUserId);
 }
