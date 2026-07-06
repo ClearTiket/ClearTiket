b89ea6c6b797b99cc5be3535a09fac777093986b
dnsrlrla <dnsrlrla7@gmail.com>
2026-07-06 18:00:36 +0900

    메인페이지 공연 예정 목록에 공연 예정 상태인 공연만 표시되도록 수정

--- src/main/java/com/clearticket/clearticket/repository/PerformanceRepository.java
+++ src/main/java/com/clearticket/clearticket/repository/PerformanceRepository.java
@@ -2,6 +2,7 @@ package com.clearticket.clearticket.repository;
 
 import com.clearticket.clearticket.model.entity.Performance;
 import com.clearticket.clearticket.model.entity.PerformanceStatus;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,6 +16,7 @@ public interface PerformanceRepository extends JpaRepository<Performance, Long> 
     List<Performance> findAllByVenueVenueIdAndStatusIn(Long venueVenueId, Collection<PerformanceStatus> statuses);
     Page<Performance> findAllByRegionIn(Collection<String> regions, Pageable pageable);
     Page<Performance> findAllByGenre(String genre, Pageable pageable);
+    List<Performance> findAllByStatusIs(PerformanceStatus status, Limit limit);
 
     Optional<Performance> findByKopisId(String kopisId);
 }
